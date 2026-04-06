e51d8dbf97b50c552abbd240e368e5a150ff833b
bahadirsina <bahadirsinaterzioglu01@gmail.com>
2026-04-06 20:40:28 +0300

    add Car

--- intro/src/main/java/com/turkcell/PgCarRepository.java
+++ intro/src/main/java/com/turkcell/PgCarRepository.java
@@ -1,5 +1,9 @@
 package com.turkcell;
 
 public class PgCarRepository {
+    public void addCar(Car car) {
+        // Veritabanına ekleme işlemi burada yapılır
+        System.out.println("Car added to PostgreSQL database: " + car.getBrand() + " " + car.getModel());
+    }
 
 }
