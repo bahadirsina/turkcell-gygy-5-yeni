7acb68f427b3d8a70cdd1fc11797672ba7a46f4d
bahadirsina <bahadirsinaterzioglu01@gmail.com>
2026-04-08 20:53:58 +0300

    update

--- spring-starter/src/main/java/com/turkcell/spring_starter/SpringStarterApplication.java
+++ spring-starter/src/main/java/com/turkcell/spring_starter/SpringStarterApplication.java
@@ -3,9 +3,10 @@ package com.turkcell.spring_starter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //Anototation => Sınıfın bir Spring Boot uygulaması olduğunu belirtir.
 public class SpringStarterApplication {
 
+	//Entrypoint
 	public static void main(String[] args) {
 		SpringApplication.run(SpringStarterApplication.class, args);
 	}
--- spring-starter/src/main/java/com/turkcell/spring_starter/controller/ProductController.java
+++ spring-starter/src/main/java/com/turkcell/spring_starter/controller/ProductController.java
@@ -4,6 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.turkcell.spring_starter.model.Product;
 
 @RestController //Anotasyon => Sınıfın bir RESTful web hizmeti denetleyicisi olduğunu belirtir.
 @RequestMapping("/api/product") // localhost:8080/api/product
@@ -16,5 +20,14 @@ public class ProductController {
     @GetMapping("hello/{name}/{age}")
     public String sayHello(@PathVariable String name, @PathVariable int age) {
         return "Hello, " + name + "! You are " + age + " years old.";
+    }
+
+    @PostMapping
+    public Product add(@RequestBody Product product){ // Json->Java objesine
+        // isim 1 haneden uzun mu?
+        // fiyat..
+        // DB'e kaydet..
+
+        return product;
     }
 }
