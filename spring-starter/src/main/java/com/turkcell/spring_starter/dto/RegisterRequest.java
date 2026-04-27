6c931ac1d81b346dcaf794871da55947f7923ef4
bahadirsina <bahadirsinaterzioglu01@gmail.com>
2026-04-27 20:59:14 +0300

    Login

---
+++ spring-starter/src/main/java/com/turkcell/spring_starter/dto/RegisterRequest.java
@@ -0,0 +1,27 @@
+package com.turkcell.spring_starter.dto;
+
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public class RegisterRequest {
+    @NotBlank
+    @Email
+    private String email;
+    @NotBlank
+    @Length(min = 6, max = 100, message = "Şifre en az 6 karakter olmalıdır, en fazla 100 karakter olabilir.")
+    private String password;
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
